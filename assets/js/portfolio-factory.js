50e62955ed2f2febf96683b01add7486963feda0
Josh Leslie <30437767+JoshMLeslie@users.noreply.github.com>
2024-05-04 13:09:19 -0400

    move non-html to assets dir
    start merging portfolio page content to home

---
+++ assets/js/portfolio-factory.js
@@ -0,0 +1,42 @@
+
+const portfolioContainer = document.querySelector('.portfolio-container');
+
+const titleFactory = ({ text }) => {
+	const title = document.createElement('h2');
+	title.textContent = text;
+	portfolioContainer.appendChild(title);
+};
+
+const portfolioItemFactory = ({link, text, imageUrl, imageAlt}) => {
+	const portfolioItem = document.createElement('a');
+	portfolioItem.href = link;
+	portfolioItem.target = '_blank';
+	portfolioItem.className = 'portfolio-item';
+
+	const titleSpan = document.createElement('span');
+	titleSpan.textContent = text;
+
+	const image = document.createElement('img');
+	image.src = imageUrl;
+	image.alt = imageAlt;
+
+	portfolioItem.appendChild(titleSpan);
+	portfolioItem.appendChild(image);
+
+	portfolioContainer.appendChild(portfolioItem);
+};
+
+(function renderPortfolioItems() {
+	fetch('assets/json/portfolio-data.json')
+		.then((res) => res.json())
+		.then((data) => {
+			data.forEach((item) => {
+				item.isTitle ?
+					titleFactory(item) :
+					portfolioItemFactory(item);
+			});
+		})
+		.catch((error) => {
+			console.error('Error fetching portfolio data:', error);
+		});
+})();
